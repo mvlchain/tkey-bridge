b540796c8f06a3e9ed679ad5187a3177337d9648
Jay Jaehwa Han <drh@wafflestudio.com>
2022-04-26 13:39:47 +0900

    fix: tkey 로드 후 share index가 3개 미만일 경우에는 new share를 하나 더 만든다

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ import {generatePrivate} from "@toruslabs/eccrypto";
 const proxyContractAddress = process.env.PROXY_CONTRACT_ADDR;
 const network = process.env.NETWORK as TORUS_NETWORK_TYPE;
 const variant = 'DEBUG';
-const version = `0.1.4-${variant}`;
+const version = `0.1.8-${variant}`;
 // @ts-ignore
 const isDebug = variant === 'DEBUG';
 
@@ -38,35 +38,40 @@ log.info("network", network)
 log.info("directWebBaseUrl", directWebBaseUrl);
 
 async function _splitKey(postboxKey: string, privateKey: BN): Promise<{ torusShare: ShareStore, deviceShare: ShareStore, serverShare: ShareStore }> {
-  log.trace('postboxkey=' + postboxKey);
+  log.debug('postboxkey=' + postboxKey);
   const serviceProvider = getServiceProvider(postboxKey);
   const tkey = new ThresholdKey({serviceProvider, storageLayer});
   const initResult = await tkey.initialize({importKey: privateKey});
   const pubkey = getPubKeyPoint(privateKey);
   if (!(pubkey.x.eq(initResult.pubKey.x) && pubkey.y.eq(initResult.pubKey.y))) {
-    log.trace('initResult.pubKey='+JSON.stringify(initResult.pubKey.toJSON()));
-    log.trace('pubkey='+JSON.stringify(pubkey.toJSON()));
+    log.debug('initResult.pubKey='+JSON.stringify(initResult.pubKey.toJSON()));
+    log.debug('pubkey='+JSON.stringify(pubkey.toJSON()));
     return _forceInitAndSplit(tkey, privateKey);
   } else if (initResult.requiredShares > 0) { // we need more shares, but we don't have it now!
     // we reset with key again
+    log.debug('initResult.requiredShares =' + initResult.requiredShares);
     return _forceInitAndSplit(tkey, privateKey);
   }
 
-  const polyId = tkey.metadata.getLatestPublicPolynomial().polynomialId;
+  let polyId = tkey.metadata.getLatestPublicPolynomial().polynomialId;
+  log.debug('polyId=' + polyId);
 
   if (tkey.getCurrentShareIndexes().length < 3) {
     log.error("share index length < 3, length=" + tkey.getCurrentShareIndexes().length);
-    throw new Error("share index length < 3");
+    const shareResults = await tkey.generateNewShare();
+    polyId = tkey.metadata.getLatestPublicPolynomial().polynomialId;
+    log.debug('new polyId=' + polyId);
+    return shareMapToShares(shareResults.newShareStores);
   }
 
   return shareMapToShares(tkey.shares[polyId]);
 }
 
 function shareMapToShares(shareStoreMap: ShareStoreMap): { torusShare: ShareStore, deviceShare: ShareStore, serverShare: ShareStore } {
-  log.trace('shareMapToShares');
+  log.debug('shareMapToShares');
   let torusShare: ShareStore, deviceShare: ShareStore, serverShare: ShareStore;
   for (let shareIndex in shareStoreMap) {
-    log.trace('shareIndex=' + shareIndex);
+    log.debug('shareIndex=' + shareIndex);
     const shareStore = shareStoreMap[shareIndex];
     if (shareIndex === '1') {
       torusShare = shareStore;
@@ -76,27 +81,27 @@ function shareMapToShares(shareStoreMap: ShareStoreMap): { torusShare: ShareStor
       serverShare = shareStore;
     }
   }
-  log.trace("torusShare=" + JSON.stringify(torusShare.toJSON()));
-  log.trace("serverShare=" + JSON.stringify(serverShare.toJSON()));
-  log.trace("deviceShare=" + JSON.stringify(deviceShare.toJSON()));
+  log.debug("torusShare=" + JSON.stringify(torusShare.toJSON()));
+  log.debug("serverShare=" + JSON.stringify(serverShare.toJSON()));
+  log.debug("deviceShare=" + JSON.stringify(deviceShare.toJSON()));
   return {torusShare, deviceShare, serverShare};
 }
 
 async function _forceInitAndSplit(tkey: ThresholdKey, privateKey: BN): Promise<{ torusShare: ShareStore, deviceShare: ShareStore, serverShare: ShareStore }> {
-  log.trace('_forceInitAndSplit');
+  log.debug('_forceInitAndSplit');
   const details = tkey.getKeyDetails();
-  log.trace('details.requiredShares=' + details.requiredShares);
+  log.debug('details.requiredShares=' + details.requiredShares);
   if (details.requiredShares === 0) {
     // don't need to generate new share!
   } else if (details.requiredShares === 1) {
     const share = new BN(generatePrivate());
     await tkey._initializeNewKey({importedKey: privateKey, determinedShare: share});
-    log.trace('after _initializeNewKey');
+    log.debug('after _initializeNewKey');
   } else {
     throw new Error('requiredShares > 1');
   }
   const polyId = tkey.metadata.getLatestPublicPolynomial().polynomialId;
-  log.trace('polyId=' + polyId);
+  log.debug('polyId=' + polyId);
   return shareMapToShares(tkey.shares[polyId]);
 }
 
@@ -130,33 +135,33 @@ async function _reconstructKeyWithTorusShare(postboxKey: string, nonProviderShar
   await tkey.initialize({ neverInitializeNewKey: true });
   tkey.inputShareStore(nonProviderShare);
   const {privKey: privateKey} = await tkey.reconstructKey();
-  log.trace('reconstructed private key=' + privateKey.toString('hex'));
+  log.debug('reconstructed private key=' + privateKey.toString('hex'));
   const polyId = tkey.metadata.getLatestPublicPolynomial().polynomialId;
-  log.trace('polyId=' + polyId);
+  log.debug('polyId=' + polyId);
   const shareStoreMap = tkey.shares[polyId];
   const indexes = tkey.metadata.getShareIndexesForPolynomial(polyId);
-  log.trace('polynomial share indexes=');
-  for (let i=0; i<indexes.length; i++) { log.trace(indexes[i]); }
+  log.debug('polynomial share indexes=');
+  for (let i=0; i<indexes.length; i++) { log.debug(indexes[i]); }
 
   const existing = Object.keys(shareStoreMap);
-  log.trace('existing indexes=');
-  for (let i=0; i<existing.length; i++) { log.trace(existing[i]); }
+  log.debug('existing indexes=');
+  for (let i=0; i<existing.length; i++) { log.debug(existing[i]); }
 
   const nonExisting = indexes.find((x) => existing.indexOf(x) < 0);
   let share: ShareStore;
-  log.trace('nonExisting=' + nonExisting);
+  log.debug('nonExisting=' + nonExisting);
   if (!!nonExisting) {
     share = tkey.outputShareStore(nonExisting, polyId);
-    log.trace('share=' + (share.share.share.toString('hex')));
+    log.debug('share=' + (share.share.share.toString('hex')));
   }
   return {privateKey, share};
 }
 
 
 async function _getTorusShare(postboxKey: string): Promise<ShareStore | null> {
-  log.trace("entering _getTorusShare");
+  log.debug("entering _getTorusShare");
   const serviceProvider = getServiceProvider(postboxKey);
-  log.trace("after init serviceProvider");
+  log.debug("after init serviceProvider");
   const tkey = new ThresholdKey({serviceProvider, storageLayer});
   try {
     await tkey.initialize({
@@ -164,10 +169,10 @@ async function _getTorusShare(postboxKey: string): Promise<ShareStore | null> {
     });
   } catch (e) {
     if (e instanceof CoreError && e.code === 1000 && e.message.includes('key has not been generated yet')) {
-      log.trace('this is new user case');
+      log.debug('this is new user case');
       return null;
     } else {
-      log.trace('tkey torus share retrieval error case');
+      log.debug('tkey torus share retrieval error case');
       throw e;
     }
   }
@@ -207,7 +212,7 @@ export function reconstructKeyWithTorusShare(postboxKey: string, shareJson: stri
 export function getTorusShare(postboxKey: string) {
   _getTorusShare(postboxKey)
     .then((ts) => {
-      _sendMessageToNative('torusShareRetrieved', ts && ts.toJSON());
+      _sendMessageToNative('torusShareRetrieved', ts && JSON.stringify(ts.toJSON()));
     })
     .catch((err) => {
       log.error('getTorusShare failed');
